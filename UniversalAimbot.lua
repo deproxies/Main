9dc7ea8283d02bb2d2901e3e8a5425ab56225e01
deproxies <deproxies@gmail.com>
2026-02-22 14:10:22 -0600

    Create UA.lua

---
+++ UniversalAimbot.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3ab5e945ade8efeead8e2d63dabcd099fe297f86e3fb5924cb883a9e6bd29b25/download"))()
